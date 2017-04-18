23fda01c7a3bc1e36f769a1fda30de3eff9a766e
ilshie <ngi@carleton.edu>
2017-04-17 20:27:46 -0500

    Add files via upload

--- climate_variability.R
+++ climate_variability.R
@@ -10,7 +10,7 @@ m <- 10000
 # minimum and maximum trend lengths to consider
 min_trend_length <- 10
 max_trend_length <- 50
-###################################################################################
+##################################################################################
 
 # Load coupled control temperature data
 if (!exists("TGlobal")) load("~/Classes/MATH400/CoupledControl.RData")
@@ -23,6 +23,9 @@ temperatures <- tail(coupled_TGlobal, 500)
 # Fit models to coupled control global temperatures
 (ar1 <- arima(temperatures, order = c(1,0,0)))
 (arma31 <- arima(temperatures, order = c(3,0,1)))
+
+# Fit white noise model to coupled control global temperatures
+(white_noise <- arima(temperatures, order = c(0,0,0)))
 
 # Function that carries out the parametric bootstrap for a given list of ARMA 
 # coefficients, standard error, and trend length; returns the standard error 
@@ -47,6 +50,11 @@ ar1_SE <- sapply(min_trend_length:max_trend_length, function(i) {
 arma31_SE <- sapply(min_trend_length:max_trend_length, function(i) {
   parametric_bootstrap(list(ar=c(1.2958, -0.7934, 0.3583), ma=-0.6278), 
                        sqrt(arma31$sigma2), i, n)
+})
+
+# obtain vector of standard errors across different trend lengths for white noise
+white_noise_SE <- sapply(min_trend_length:max_trend_length, function(i) {
+  parametric_bootstrap(list(ar=0), sqrt(white_noise$sigma2), i, n)
 })
 
 ############################################################################################
@@ -91,13 +99,14 @@ df <- slope_matrix %>%
 # add SE derived from ARMA models to data frame
 df$AR1 <- ar1_SE
 df$ARMA31 <- arma31_SE
+df$white <- white_noise_SE
 
 # plot computed standard errors against trend length
-df %>% gather(true_mean:ARMA31, key=type, value=SE) %>%
+df %>% gather(true_mean:white, key=type, value=SE) %>%
   ggplot(aes(x=trend_length, y=SE)) +
     geom_line(aes(color=type, linetype=type)) +
-    scale_color_manual(values=c(2,3,1,1,1)) +
-    scale_linetype_manual(values=c(1,1,2,1,2)) +
+    scale_color_manual(values=c(2,3,1,1,1,4)) +
+    scale_linetype_manual(values=c(1,1,2,1,2,1)) +
     labs(x="\nTrend length (year)", y="Estimated standard error\n") +
     scale_y_continuous(breaks=seq(0, 0.015, 0.003)) +
     theme(axis.title=element_text(size=16), axis.text=element_text(size=14),
@@ -106,15 +115,16 @@ df %>% gather(true_mean:ARMA31, key=type, value=SE) %>%
 # plot ratio of ARMA standard error to true standard error against trend length
 df2 <- df %>% transmute(trend_length, 
     AR1_mean=AR1/true_mean, AR1_upper=AR1/true_upper, AR1_lower=AR1/true_lower, 
-    ARMA31_mean=ARMA31/true_mean, ARMA31_upper=ARMA31/true_upper, ARMA31_lower=ARMA31/true_lower)
+    ARMA31_mean=ARMA31/true_mean, ARMA31_upper=ARMA31/true_upper, ARMA31_lower=ARMA31/true_lower,
+    white_mean=white/true_mean, white_upper=white/true_upper, white_lower=white/true_lower)
 
-p <- df2 %>% gather(AR1_mean:ARMA31_lower, key=model, value=SE) %>%
+p <- df2 %>% gather(AR1_mean:white_lower, key=model, value=SE) %>%
   ggplot(aes(x=trend_length, y=SE)) +
     geom_line(aes(color=model, size=model)) +
     geom_line(y=1, linetype=2) +
-    scale_color_manual(breaks=c("AR1_mean", "ARMA31_mean"), values=c(2,2,2,3,3,3),
-                       labels=c("AR1", "ARMA31")) +
-    scale_size_manual(values=rep(c(0.2,1.2,0.2),2), guide=FALSE) +
+    scale_color_manual(breaks=c("AR1_mean", "ARMA31_mean", "white_mean"), 
+                       values=rep(c(2,3,4), each=3), labels=c("AR1", "ARMA31", "white noise")) +
+    scale_size_manual(values=rep(c(0.2,1.2,0.2),3), guide=FALSE) +
     labs(x="\nTrend length (year)", y="Ratio of estimated to true standard error\n") +
     theme(axis.title=element_text(size=16), axis.text=element_text(size=14),
           legend.title=element_text(size=16), legend.text=element_text(size=14))
@@ -126,4 +136,7 @@ p + geom_ribbon(data=df3, inherit.aes=FALSE,
                 fill=2, alpha=0.1) +
   geom_ribbon(data=df3, inherit.aes=FALSE,
               aes(x=trend_length, ymin=ARMA31_lower, ymax=ARMA31_upper),
-              fill=3, alpha=0.1)
+              fill=3, alpha=0.1) +
+  geom_ribbon(data=df3, inherit.aes=FALSE,
+              aes(x=trend_length, ymin=white_lower, ymax=white_upper),
+              fill=4, alpha=0.1)
